a063097f354e1b24d152711015cbc16d713958d6
Davi França Emmerick de Souza <daviemmerick2019@gmail.com>
2026-09-18 11:54:23 -0300

    [Feat] Entidades: restaurante, endereço e entrega v0

--- src/entities/__init__.py
+++ src/entities/__init__.py
@@ -1,3 +1,6 @@
 from .cliente import Cliente
+from .endereco import Endereco
+from .entrega import Entrega
 from .pedido import Pedido
 from .produto import Produto
+from .restaurante import Restaurante
